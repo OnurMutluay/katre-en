eb0e9ebb055e0841f8a2d61e9cdfa90a096de3d7
OnurMutluay <onur.mutluay@rubrec.com>
2025-08-01 23:10:19 +0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -73,7 +73,7 @@ export const Navbar = () => {
               <SheetContent side={"left"}>
                 <SheetHeader>
                   <SheetTitle className="font-bold text-xl">
-                    Shadcn/React
+                    KATRE CONSULTANCY
                   </SheetTitle>
                 </SheetHeader>
                 <nav className="flex flex-col justify-center items-center gap-2 mt-4">
